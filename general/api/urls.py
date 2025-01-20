22a8b44d98d687ff6df23f9818796cc18f3ccd4c
Азат Янбердин <azat@MacBook-Air.local>
2025-01-21 01:11:29 +0500

    add new endpoints

--- general/api/urls.py
+++ general/api/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import SimpleRouter
 
-from general.api.views import CommentsViewSet, PostViewSet, UserViewSet
+from general.api.views import ChatViewSet, CommentsViewSet, PostViewSet, ReactionViewSet, UserViewSet
 
 
 router = SimpleRouter()
 router.register(r'users', UserViewSet, basename="users")
 router.register(r'posts', PostViewSet, basename="posts")
 router.register(r'comments', CommentsViewSet, basename="comments")
+router.register(r'reaction', ReactionViewSet, basename="reaction")
+router.register(r'chats', ChatViewSet, basename="chats")
 
 urlpatterns = router.urls
