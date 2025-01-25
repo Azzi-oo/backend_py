03b91713483a9fcb7a48eb663e5b264874fd6638
Азат Янбердин <azat@MacBook-Air.local>
2025-01-25 16:06:38 +0500

    add new tests for comments

--- general/api/tests/test_comments.py
+++ general/api/tests/test_comments.py
@@ -2,7 +2,7 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 import json
 
-from general.factories import PostFactory, ReactionFactory, UserFactory
+from general.factories import CommentFactory, PostFactory, ReactionFactory, UserFactory
 from general.models import Comment, Post, Reaction
 
 
@@ -42,3 +42,39 @@ class CommentTestCase(APITestCase):
             format="json",
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+    def test_delete_own_comment(self):
+        comment = CommentFactory(author=self.user)
+        response = self.client.delete(
+            f"{self.url}{comment.pk}/",
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Comment.objects.count(), 0)
+        
+    def test_delete_other_comment(self):
+        comment = CommentFactory()
+        response = self.client.delete(
+            f"{self.url}{comment.pk}/",
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Comment.objects.count(), 1)
+        self.assertEqual(
+            response.data["detail"],
+            "Вы не являетесь автором этого коммента.",
+        )
+        
+    def test_comment_list_filtered_by_post_id(self):
+        comments = CommentFactory.create_batch(5, post=self.post)
+        
+        CommentFactory.create_batch(5)
+        
+        url = f"{self.url}?post__id={self.post.pk}"
+        response = self.client.get(path=url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]), 5)  
+        
+        comments_ids = {comment.id for comment in comments}
+        for comment in response.data["results"]:
+            self.assertTrue(comment["id"] in comments_ids)
