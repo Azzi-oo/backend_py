290e70151b2fbe3eb040e28e55e0a68c07d267e9
Азат Янбердин <azat@MacBook-Air.local>
2025-01-25 14:20:23 +0500

    add new tests fot posts

--- config/settings.py
+++ config/settings.py
@@ -84,6 +84,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': "%Y-%m-%dT%H:%M:%S",
 }
 
 AUTH_USER_MODEL = 'general.User'
